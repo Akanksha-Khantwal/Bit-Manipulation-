a5b95ac9c14ebe6e9d1c7c63852076acf0b41037
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2025-10-31 22:08:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+    int count =0;
+     while(n>0){
+        if(x&1)
+        count ++;
+        x>>=1;
+     }
+     return count;
+
+
+        
+    }
+};
